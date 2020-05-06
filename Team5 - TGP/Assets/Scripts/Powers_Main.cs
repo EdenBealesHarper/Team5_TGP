95786e22fea25a1665930cf5386255333f516142
Eden <eden.bealesharper@gmail.com>
2020-05-06 14:30:38 +0100

    Added script for upgrade slotting
    Need to transfer code to the player script when that is implemented

---
+++ Team5 - TGP/Assets/Scripts/Powers_Main.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Powers_Main : MonoBehaviour
+{
+    public List<string> slottedUpgrades = new List<string>();
+    public List<string> knownUpgrades = new List<string>();
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        foreach (string upgrade in slottedUpgrades)
+        {
+            if (upgrade == "SPEED20")
+            {
+                print("SPEED20");
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.StartsWith("PU_"))
+        {
+            string nameNoPU = other.gameObject.name;
+            nameNoPU.Replace("PU_", string.Empty);
+            knownUpgrades.Add(nameNoPU);
+        }
+    }
+
+    public void slotUpgrade(string upgradeName)
+    {
+        if (!slottedUpgrades.Contains(upgradeName))
+        {
+            slottedUpgrades.Add(upgradeName);
+        }
+        else 
+        {
+            print("Upgrade Already Slotted");
+        }
+    }
+}
+
