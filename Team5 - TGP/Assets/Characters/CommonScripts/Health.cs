22f7c52b7fe1051e69c90a84a9961d6c13bb30bf
RinKail <35337657+RinKail@users.noreply.github.com>
2020-05-18 14:38:15 +0100

    Fixed player movement modifier

---
+++ Team5 - TGP/Assets/Characters/CommonScripts/Health.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    protected float CurrentHealth;
+    protected float TotalHealth;
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TakeDamage(float Damage)
+    {
+        CurrentHealth -= Damage;
+        if (CurrentHealth <= 0)
+        {
+            //ToDo On Death event
+        }
+    }
+
+    void UpdateHealthBar()
+    {
+
+    }
+}
