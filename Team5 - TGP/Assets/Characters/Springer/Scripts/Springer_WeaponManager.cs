99645f7a6b7fd1a0c95d943552ac3f948d0175d7
RinKail <35337657+RinKail@users.noreply.github.com>
2020-06-05 19:21:19 +0100

    Updated equip function

--- Team5 - TGP/Assets/Characters/Springer/Scripts/Springer_WeaponManager.cs
+++ Team5 - TGP/Assets/Characters/Springer/Scripts/Springer_WeaponManager.cs
@@ -7,7 +7,7 @@ public class Springer_WeaponManager : MonoBehaviour
 
 
     [SerializeField]
-    Weapon[] AvailableWeapons;
+    List<Weapon> AvailableWeapons = new List<Weapon>();
 
     [SerializeField]
     GameObject MeshObject;
@@ -60,7 +60,7 @@ public class Springer_WeaponManager : MonoBehaviour
     {
         MR = MeshObject.GetComponent<MeshRenderer>();
         MF = MeshObject.GetComponent<MeshFilter>();
-        if (AvailableWeapons.Length > 0)
+        if (AvailableWeapons.Count > 0)
         {
             SetWeapon(0);
         }
@@ -168,9 +168,9 @@ public class Springer_WeaponManager : MonoBehaviour
 
     }
 
-    public void EquipWeapon()
+    public void EquipWeapon(Weapon NewWeapon)
     {
-
+        AvailableWeapons.Add(NewWeapon);
     }
 
     public void DropWeapon()
@@ -180,7 +180,7 @@ public class Springer_WeaponManager : MonoBehaviour
 
     void SwitchWeapon(int Direction)
     {
-        ActiveWeapon += ((Direction + ActiveWeapon) % AvailableWeapons.Length);
+        ActiveWeapon += ((Direction + ActiveWeapon) % AvailableWeapons.Count);
          MF.mesh = AvailableWeapons[ActiveWeapon].WeaponMesh;
         Projectile = AvailableWeapons[ActiveWeapon].Projectile;
         WeaponMode = AvailableWeapons[ActiveWeapon].WeaponType;
