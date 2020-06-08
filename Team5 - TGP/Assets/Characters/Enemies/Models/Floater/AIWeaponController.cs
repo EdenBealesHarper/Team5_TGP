8b23c8503ebf0a26e45f277aa9f1273175e2fbd1
RinKail <35337657+RinKail@users.noreply.github.com>
2020-06-08 22:50:09 +0100

    Added Walker AI model / Animations
    Walker AI mesh object added.
    
    To set up. Attatch the new prefab to the preset AI prefab, Walker needs the object with the rigidybody to to be assignned to it's rigidbody target.
    
    Player must be Target for both AI types.
    
    Call DoAttack from AI_Walker to perform walker's attack.
    
    Floating Enemy automatically targets player and shoots when within range. Can set target range and fire rate from prefab.

---
+++ Team5 - TGP/Assets/Characters/Enemies/Models/Floater/AIWeaponController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIWeaponController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject ProjectileObject;
+
+    [SerializeField]
+    GameObject ProjectilePosition;
+
+    [SerializeField]
+    GameObject Cannon;
+
+    [SerializeField]
+    GameObject Target;
+
+    [SerializeField]
+    float ActiveRange;
+
+    [SerializeField]
+    float FireRate;
+
+    float FireTimer;
+
+    bool bCanFire = true;
+
+    float Direction = 1;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FindTarget();
+        if (FireTimer <= 0)
+        {
+            FireWeapon();
+            FireTimer = FireRate;
+        }
+        else
+        {
+            FireTimer -= Time.deltaTime;
+        }
+    }
+
+
+    void FindTarget()
+    {
+        if (Vector2.Distance(transform.position, Target.transform.position) < ActiveRange)
+        {
+            Cannon.transform.LookAt(Target.transform.position);
+
+        }
+    }
+
+
+    void FireWeapon()
+    {
+        GameObject Proj = Instantiate(ProjectileObject, ProjectilePosition.transform.position, ProjectilePosition.transform.rotation);
+        Proj.GetComponent<ProjectileBase>().SetDirection((int)Direction);
+    }
+}
---
+++ Team5 - TGP/Assets/Characters/Enemies/Models/Walkier/AI_Walker.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Walker : MonoBehaviour
+{
+    [SerializeField]
+    GameObject RigidBodyObject;
+
+    [SerializeField]
+    GameObject Target;
+
+    Animator Anim;
+    Rigidbody2D rb;
+
+    Health TargetHealth;
+
+
+
+
+    bool bAttackTriggered = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Anim = gameObject.GetComponent<Animator>();
+         rb = RigidBodyObject.GetComponent<Rigidbody2D>();
+
+        TargetHealth=  Target.GetComponent<Health>();
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         Anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+
+
+      
+
+        if (bAttackTriggered)
+        {
+            Anim.ResetTrigger("Attack");
+            bAttackTriggered = false;
+        }
+        
+
+
+    }
+
+
+
+   public void DoAttack()
+    {
+        Anim.SetTrigger("Attack");
+        bAttackTriggered = true;
+        TargetHealth.TakeDamage(10);
+
+    }
+}
